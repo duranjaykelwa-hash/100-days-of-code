814ad133bbd3ef1fc394f59dfe633610b23d63c8
duranjaykelwa-hash <duranjaykelwa@gmail.com>
2026-08-18 19:31:23 +0530

    Add files via upload

---
+++ 100_days _of _code/DAY7_1.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+int num;
+printf("Enter an integer: ");
+scanf("%d", &num);
+if (num >= 0) 
+{
+if (num == 0)
+printf("Zero");
+else printf("Positive");
+}
+ else 
+{
+printf("Negative");
+}
+}
