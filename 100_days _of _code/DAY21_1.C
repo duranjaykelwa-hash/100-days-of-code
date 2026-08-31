86390673e88fbc72a9825cb91a989317553e507b
duranjaykelwa-hash <duranjaykelwa@gmail.com>
2026-08-31 20:59:37 +0530

    Add files via upload

---
+++ 100_days _of _code/DAY21_1.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+int n, first, last, digits = 1, temp, result;
+printf("Enter a number: ");
+scanf("%d", &n);
+temp = n;
+last = n % 10;
+while (temp >= 10) 
+{
+temp = temp / 10;
+digits = digits * 10;
+}
+first = temp;
+result = last * digits + (n % digits) / 10 * 10 + first;
+printf("Number after swapping first and last digit: %d", result);
+}
