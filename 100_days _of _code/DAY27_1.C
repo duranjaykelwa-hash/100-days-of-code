01eed6b25fd598291ee68adff304b11fdd05ea8f
duranjaykelwa-hash <duranjaykelwa@gmail.com>
2026-09-07 20:01:49 +0530

    Add files via upload

---
+++ 100_days _of _code/DAY27_1.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+int i, j, stars;
+for (i = 1; i <= 9; i++) 
+{
+if (i <= 5)
+stars = 2 * i - 1;
+else
+stars = 2 * (9 - i) + 1;
+for (j = 1; j <= stars; j++) 
+{
+printf("*");
+}
+printf("\n");
+}
+}
