55fc171aaec1edae4161cf1a64dfb04e39b649b4
duranjaykelwa-hash <duranjaykelwa@gmail.com>
2026-09-09 15:07:13 +0530

    Add files via upload

---
+++ 100_days _of _code/DAY30_2.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() 
+{
+int n, i;
+int positive = 0, negative = 0, zero = 0;
+int arr[100];
+printf("Enter the number of elements: ");
+scanf("%d", &n);
+printf("Enter the elements:\n");
+for (i = 0; i < n; i++)
+{
+scanf("%d", &arr[i]);
+if (arr[i] > 0)
+positive++;
+else if (arr[i] < 0)
+negative++;
+else
+zero++;
+}
+printf("Positive numbers = %d\n", positive);
+printf("Negative numbers = %d\n", negative);
+printf("Zero elements = %d\n", zero);
+}
