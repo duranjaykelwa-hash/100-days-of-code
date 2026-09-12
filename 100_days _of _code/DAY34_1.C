811035f87bea0ae8093fb7845f44213b47deb5d0
duranjaykelwa-hash <duranjaykelwa@gmail.com>
2026-09-12 21:29:45 +0530

    Day 34: Solved Q67 & Q68 (Arrays (1D))

---
+++ 100_days _of _code/DAY34_1.C
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() 
+{
+int a[100], n, pos, x;
+int i;
+printf("Enter number of elements: ");
+scanf("%d", &n);
+printf("Enter array elements:\n");
+for(i = 0; i < n; i++) 
+{
+scanf("%d", &a[i]);
+}
+printf("Enter position: ");
+scanf("%d", &pos);
+printf("Enter element: ");
+scanf("%d", &x);
+for(i = n; i >= pos; i--) 
+{
+a[i] = a[i - 1];
+}
+a[pos - 1] = x;
+n++;
+printf("Array after insertion:\n");
+for(i = 0; i < n; i++) 
+{
+printf("%d ", a[i]);
+}
+}
